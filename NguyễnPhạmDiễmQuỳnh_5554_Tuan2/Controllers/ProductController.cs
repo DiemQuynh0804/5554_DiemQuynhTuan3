83f6a9229a4c0c1569e5c2e79ca4ae1c7041839b
Administrator <Administrator@10PC314>
2024-03-22 16:26:23 +0700

    Add project files.

---
+++ NguyễnPhạmDiễmQuỳnh_5554_Tuan2/Controllers/ProductController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NguyễnPhạmDiễmQuỳnh_5554_Tuan2.Models;
+
+namespace NguyễnPhạmDiễmQuỳnh_5554_Tuan2.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productRepository.GetAll();
+            return View(products);
+        }
+
+        //add
+        public IActionResult Add()
+        {
+            var categories = _categoryRepository.GetAllCategory();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.Add(product);
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        //display
+        public IActionResult Display(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        //update
+        public IActionResult Update(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productRepository.Update(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //delete
+        public IActionResult Delete(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _productRepository.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
